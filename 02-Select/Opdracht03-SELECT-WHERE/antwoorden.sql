18efd55282f471be16289b4635199f7098bd587d
Shockstick <2152301@talnet.nl>
2022-01-19 10:55:44 +0100

    opdracht 03 - SELECT WHERE

--- 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
+++ 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
@@ -1,19 +1,20 @@
 -- Opdracht 1 
-
+SELECT * FROM jaar2015 WHERE rank=7;
 -- Opdracht 2 
-
+SELECT * FROM `jaar2016` WHERE rank=7;
 -- Opdracht 3
-
+SELECT * FROM `jaar2015` WHERE score=7378;
 -- Opdracht 4
-
+SELECT * FROM `jaar2016` WHERE score=7339;
 -- Opdracht 5
-
+SELECT * FROM `jaar2015` WHERE region='North America';
 -- Opdracht 6 
-
+SELECT * FROM `jaar2016` WHERE rank>25;
 -- Opdracht 7 
-
+SELECT * FROM `jaar2016` WHERE score>7087;
 -- Opdracht 8
-
+SELECT * FROM `jaar2016` WHERE rank<5;
 -- Opdracht 9
-
+SELECT * FROM `jaar2015` WHERE score=694;
 -- Opdracht 10
+SELECT * FROM `jaar2016` WHERE region='Latin America and Caribbean';
