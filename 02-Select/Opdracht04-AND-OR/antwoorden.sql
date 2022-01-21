46690a440066be20a5ef96e3748d629896e9583e
Shockstick <2152301@talnet.nl>
2022-01-21 10:53:09 +0100

    SELECT

--- 02-Select/Opdracht04-AND-OR/antwoorden.sql
+++ 02-Select/Opdracht04-AND-OR/antwoorden.sql
@@ -1,15 +1,15 @@
 -- Opdracht 1 
-
+Cesc F bregas, Diego Costa, Azpilicueta, Pedro, Marcos Alonso, Morata, 
 -- Opdracht 2 
-
+Brahim D az, Jorge Cuenca, Abel Ruiz, Miranda,
 -- Opdracht 3
-
+J. Milner, D. Sturridge, S. Mignolet, A. Lallana, G. Wijnaldum, J. Henderson, A. Bogd n, Coutinho, N. Clyne, D. Lovren, D. Ings, J. Matip, A. Oxlade-Chamberlain, Roberto Firmino, J. Flanagan, L. Karius, Alberto Moreno, D. Ward, E. Can, S. Man, M. Salah, L. Markovi?, L. Jones, A. Robertson, C. Brannagan     
 -- Opdracht 4
-
+K. Huntelaar, S. de Jong, N. Viergever, B. van Leer, J. Veltman, M. Dijks, D. Sinkgraven, A. Nouri, D. van de Beek, N. Alblas, F. de Jong, D. Dankerlui, C. Eiting, M. de Ligt, D. Zeefuik, J. Kluivert
 -- Opdracht 5
-
+L. Sch ne, K. Lamprou, A. Younes, H. Ziyech, V. ?ern, L. Orejuela, A. Onana, M. Cassierra, K. Dolberg, M. W ber
 -- Opdracht 6 
-
+SELECT * FROM `players` WHERE club = "AZ Alkmaar" AND age;
 -- Opdracht 7 
 
 -- Opdracht 8
