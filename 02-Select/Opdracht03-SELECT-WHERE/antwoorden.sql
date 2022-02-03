2f73c0da25a3ed2cac3505929140f5f69fcdb884
Shockstick <2152301@talnet.nl>
2022-02-03 13:09:27 +0100

    Fix

--- 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
+++ 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
@@ -1,15 +1,15 @@
 -- Opdracht 1 
-SELECT * FROM jaar2015 WHERE rank=7;
+SELECT * FROM 'jaar2015' WHERE rank=7;
 -- Opdracht 2 
-SELECT * FROM `jaar2016` WHERE rank=7;
+SELECT * FROM `jaar2016` WHERE country = 'Netherlands';
 -- Opdracht 3
-SELECT * FROM `jaar2015` WHERE score=7378;
+SELECT * FROM `jaar2015` WHERE country = 'Netherlands';
 -- Opdracht 4
-SELECT * FROM `jaar2016` WHERE score=7339;
+SELECT * FROM `jaar2016` WHERE country = 'Netherlands';
 -- Opdracht 5
 SELECT * FROM `jaar2015` WHERE region='North America';
 -- Opdracht 6 
-SELECT * FROM `jaar2016` WHERE rank>25;
+SELECT * FROM `jaar2016` WHERE rank >= 25;
 -- Opdracht 7 
 SELECT * FROM `jaar2016` WHERE score>7087;
 -- Opdracht 8
@@ -17,4 +17,4 @@ SELECT * FROM `jaar2016` WHERE rank<5;
 -- Opdracht 9
 SELECT * FROM `jaar2015` WHERE score=694;
 -- Opdracht 10
-SELECT * FROM `jaar2016` WHERE region='Latin America and Caribbean';
+SELECT * FROM `jaar2016` WHERE region='Latin America and Caribbean'; 
