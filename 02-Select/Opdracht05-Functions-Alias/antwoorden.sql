8b00888153556e3c0a44afb6c8e612aad760b6ba
Shockstick <2152301@talnet.nl>
2022-02-03 14:22:02 +0100

    Fix

--- 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
+++ 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
@@ -1,9 +1,9 @@
 -- Opdracht 1 
 SELECT Max(wage) as hoogste_loon_speler_fc_utrecht FROM `players` WHERE club = 'FC utrecht';
 -- Opdracht 2 
-SELECT Count(wage) as afgeronde_gemiddelde_inkomen_wage FROM `players` WHERE value;
+SELECT ROUND(wage) as afgeronde_gemiddelde_inkomen_wage FROM `players` WHERE wage;
 -- Opdracht 3
-SELECT Count(value) as Loon_FC_Groningen FROM `players` WHERE club = 'FC Groningen';
+SELECT SUM(value) as Loon_FC_Groningen FROM `players` WHERE club = 'FC Groningen';
 -- Opdracht 4
  SELECT Count(*) As Manchester_City_and_United_spelers_aantal FROM players WHERE club = "Manchester City" OR "Manchester United";
 -- Opdracht 5
@@ -13,8 +13,8 @@ SELECT AVG(wage) As Income_age_20 FROM `players` WHERE age < 20;
 -- Opdracht 7 
 SELECT AVG(wage) As Income_age_20 FROM `players` WHERE age > 20;
 -- Opdracht 8
-SELECT MAX(value) As value_players_Chelsea FROM `players` WHERE club = 'Chelsea';
+SELECT ROUND(value) As value_players_Chelsea FROM `players` WHERE club = 'Chelsea';
 -- Opdracht 9
-SELECT ROUND(age) As afgeronde_leeftijd FROM `players` WHERE 1;
+SELECT AVG(age) As afgeronde_leeftijd FROM `players` WHERE 1;
 -- Opdracht 10
-SELECT club, MAX(wage) as wage, ROUND(value) as value FROM `players` WHERE club = 'Liverpool';
+SELECT club, ROUND(wage) as wage, ROUND(value) as value FROM `players` WHERE club = 'Liverpool';
