c2784c416d06ffd9f17ba01d915638b22a75a7b8
Shockstick <2152301@talnet.nl>
2022-02-04 09:17:14 +0100

    Fix

--- 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
+++ 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
@@ -1,6 +1,6 @@
 -- Opdracht 3
 DELETE FROM `players` WHERE id = 198329
 -- Opdracht 4
-DELETE FROM `players` WHERE id =
+DELETE FROM `players` WHERE id = 164281
 -- Opdracht 5
-DELETE FROM `players` WHERE id =
+DELETE FROM `players` WHERE id = 169663
