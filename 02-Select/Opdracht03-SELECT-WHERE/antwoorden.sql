c7ff4a99fc9a52634cda22d7117f372231def380
Shockstick <2152301@talnet.nl>
2022-02-11 10:44:12 +0100

    Fix

--- 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
+++ 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
@@ -13,7 +13,7 @@ SELECT country FROM `jaar2016` WHERE rank = 25;
 -- Opdracht 7 
 SELECT country FROM `jaar2016` WHERE score > 7087;
 -- Opdracht 8
-SELECT country, region FROM `jaar2016` WHERE rank<5 OR rank=5;;
+SELECT country, region FROM `jaar2016` WHERE rank<=5;;
 -- Opdracht 9
 SELECT score FROM `jaar2015` WHERE country = 'Ireland';
 -- Opdracht 10
